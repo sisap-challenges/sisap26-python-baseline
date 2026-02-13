c081a5a3f656c1bd5c7bd5bda2a19b47a494e880
Martin Aumüller <maau@itu.dk>
2026-02-13 14:02:55 +0100

    updated to new file format

--- datasets.py
+++ datasets.py
@@ -4,37 +4,35 @@ from urllib.request import urlretrieve
 from pathlib import Path
 
 def download(src, dst):
+    print(dst)
     if not os.path.exists(dst):
         os.makedirs(Path(dst).parent, exist_ok=True)
         print('downloading %s -> %s...' % (src, dst))
         urlretrieve(src, dst)
 
 def get_fn(dataset, task):
-    return os.path.join("data", dataset, task, f"{dataset}.h5"), os.path.join('data', dataset, task, 'gt', f'gt_{dataset}.h5')
+    return os.path.join("data", dataset, task, f"{dataset}.h5")
 
 def prepare(dataset, task):
     url = DATASETS[dataset][task]['url']
-    gt_url = DATASETS[dataset][task]['gt_url']
-    fn, gt_fn = get_fn(dataset, task)
+    fn = get_fn(dataset, task)
 
     download(url, fn)
-    download(gt_url, gt_fn)
 
 def get_query_count(dataset, task):
-    fn, _ = get_fn(dataset, task) 
+    fn = get_fn(dataset, task) 
     f = h5py.File(fn)
     qn = len(DATASETS[dataset][task]['queries'](f))
     f.close()
     return qn
 
 DATASETS = {
-    'llama': {
+    'llama-dev': {
         'task2' : {
             'url': 'https://huggingface.co/datasets/vector-index-bench/vibe/resolve/main/llama-128-ip.hdf5',
-            'queries': lambda x: x['test'],
+            'queries': lambda x: x['test']['queries'],
             'data': lambda x: x['train'],
-            'gt_url': 'https://huggingface.co/datasets/vector-index-bench/vibe/resolve/main/llama-128-ip.hdf5',
-            'gt_I': lambda x: x['neighbors'],
+            'gt_I': lambda x: x['test']['knns'],
             'k': 30,
         }
     }
